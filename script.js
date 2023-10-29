45a0ec9b1aac88a82dbbe40a9ce1995f5174d6ec
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-10-29 21:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // your code here
-function updateURL() {
+ function updateURL() {
             var name = document.getElementById("name").value;
             var year = document.getElementById("year").value;
             
             var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
 	  document.getElementById("url").textContent = url;
  }
+ 
