6292135b0234c6d9a4b4f9ccac24de6f297e3f15
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-11-01 11:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ document.getElementById('button').addEventListener( "click",e => {
             
             var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
 	  document.getElementById("url").textContent = url;
-}
+	
+})
  // function updateURL(e) {
 	//  e.preventDefault();
  //            var name = document.getElementById("name").value;
