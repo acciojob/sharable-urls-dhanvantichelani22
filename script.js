1aefa9d5767d939e45d3872cc055fea0359d75f8
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-11-01 11:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // your code here
- function updateURL() {
+document.getElementById('button').addEventListener(e => {
+    e.preventDefault();
             var name = document.getElementById("name").value;
+	 
             var year = document.getElementById("year").value;
             
             var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
 	  document.getElementById("url").textContent = url;
- }
+}
+ // function updateURL(e) {
+	//  e.preventDefault();
+ //            var name = document.getElementById("name").value;
+	 
+ //            var year = document.getElementById("year").value;
+            
+ //            var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+	//   document.getElementById("url").textContent = url;
+ // }
  
